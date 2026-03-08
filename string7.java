67bf65c00fd4f462260868c9d4d75c61dca7521c
hmvinay29 <hmvinay2025@gmail.com>
2026-03-08 15:27:42 +0530

    Add files via upload

---
+++ string7.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class string7 {
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Enter the first string:");
+		String a = scanner.next();
+		System.out.println("Enter the Second string:");
+		String b= scanner.next();
+		System.out.println(a+b);
+	}
+}		
