5e7324d9c4e8d47b15650c56880f0bb94e9b46b4
hmvinay29 <hmvinay2025@gmail.com>
2026-03-09 05:32:19 +0530

    Add files via upload

---
+++ string8.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class string8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first string:");
+		String a=scanner.nextLine();
+		System.out.println("Enter the second string:");
+		String b=scanner.nextLine();
+		
+		System.out.println(a.startsWith(b));
+	}
+
+}
