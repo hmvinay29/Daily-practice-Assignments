cad2f5c90c6dc9ebcb24731a226ab672a5c2b4f5
hmvinay29 <hmvinay2025@gmail.com>
2026-03-09 05:47:34 +0530

    Add files via upload

---
+++ char1.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+import java.util.Scanner;
+
+import Dailypracticecodes.scanner;
+
+public class char1 {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the character:");
+		char a = scanner.next().charAt(0);
+		System.out.println(a);
+	}
+
+}
