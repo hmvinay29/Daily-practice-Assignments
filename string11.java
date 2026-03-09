d211c0495bf15758205a615f2464103ee352f792
hmvinay29 <hmvinay2025@gmail.com>
2026-03-09 06:19:30 +0530

    Add files via upload

---
+++ string11.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class string11 {
+	public static void main(String[] args) {	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String a =scanner.nextLine();
+		System.out.println(a.trim());
+}
+}
